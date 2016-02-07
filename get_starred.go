8f3d19f79fb4d4ff68bf9a0cec18f5b4e030185e
Joe Gallo <joe@brassafrax.com>
2016-02-06 23:16:47 -0500

    pagination

--- get_starred.go
+++ get_starred.go
@@ -3,14 +3,45 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"math"
 	"net/http"
 	"os"
+	"strconv"
 
 	"github.com/google/go-github/github"
 	"golang.org/x/oauth2"
 )
 
+func main() {
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":4000", nil)
+}
+
 func handler(w http.ResponseWriter, r *http.Request) {
+	start, err := strconv.Atoi(r.URL.Query().Get("start"))
+	if err != nil {
+		start = math.MaxInt32
+	}
+
+	popularRepos := getPopularRepos(start)
+
+	lastRepo := popularRepos[len(popularRepos)-1]
+	lastStars := *lastRepo.StargazersCount
+
+	// print list
+	fmt.Fprint(w, "<html><head></head><body>")
+	fmt.Fprint(w, "<ul>")
+	for _, repo := range popularRepos {
+		fmt.Fprintf(w, "<li><a href=\"%s\">%s/%s (%d)</a></li>",
+								*repo.HTMLURL, *repo.Owner.Login, *repo.Name,
+								*repo.StargazersCount)
+	}
+	fmt.Fprint(w, "</ul>")
+	fmt.Fprintf(w, "<div style=\"padding-left: 40px;\"><a href=\"%s/?start=%d\">Next</a></div>", r.URL.Host, lastStars)
+	fmt.Fprint(w, "</body></html>")
+}
+
+func getPopularRepos(start int) []github.Repository {
 	dat, err := ioutil.ReadFile("auth_token.txt")
 	if err != nil {
 		fmt.Println(err)
@@ -57,13 +88,8 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	var popularRepos []github.Repository
-	maxStars := 999999999
-	minStars := 1000
-	stars := maxStars
-	q := ""
+	q := fmt.Sprintf("stars:<=%d", start)
 	for {
-		q = fmt.Sprintf("stars:<=%d", stars)
-		fmt.Printf("%s %d\n", q, popOpt.ListOptions.Page)
 		searchResults, resp, err := client.Search.Repositories(q, popOpt)
 		if err != nil {
 			fmt.Println(err)
@@ -75,13 +101,6 @@ func handler(w http.ResponseWriter, r *http.Request) {
 
 		// get next page
 		if resp.NextPage == 0 {
-			lastRepo := popularRepos[len(popularRepos)-1]
-			lastStars := *lastRepo.StargazersCount
-			if lastStars >= minStars {
-				stars = lastStars
-				popOpt.ListOptions.Page = 0
-				continue
-			}
 			break
 		}
 		popOpt.ListOptions.Page = resp.NextPage
@@ -94,20 +113,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
 		}
 	}
 
-	// print list
-	fmt.Fprint(w, "<html><head></head><body>")
-	fmt.Fprint(w, "<ul>")
-	for _, repo := range popularRepos {
-		fmt.Fprintf(w, "<li><a href=\"%s\">%s/%s (%d)</a></li>",
-								*repo.HTMLURL, *repo.Owner.Login, *repo.Name,
-								*repo.StargazersCount)
-	}
-	fmt.Fprint(w, "</ul></body></html>")
-}
-
-func main() {
-	http.HandleFunc("/", handler)
-	http.ListenAndServe(":4000", nil)
+	return popularRepos
 }
 
 func contains(s []github.Repository, e github.Repository) bool {
